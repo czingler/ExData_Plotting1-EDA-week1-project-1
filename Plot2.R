ddee8ef9367b279b4bd9299fe4d9346c1cbcc2e2
czingler <charles@zingler.biz>
2016-07-04 14:26:00 +1000

    Add files via upload

---
+++ Plot2.R
@@ -0,0 +1,19 @@
+## czingler 3.7.2016
+## Plot2.R
+
+## Data set Electric power consumption zip file downloaded, 
+## and unzipped in working directory before scripts run
+## check file in working directory using dir()
+
+datafile <- "./household_power_consumption.txt"
+data <- read.table(datafile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#viewdata head(subSetData)
+#dimdata dim(subSetData)
+
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+global_active_power <- as.numeric(subSetData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
